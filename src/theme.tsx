602d4f5c505b359249907cb385a3e6f163f8c0e4
Cameron Leconte <cle@yuzucorp.com>
2024-02-01 13:58:27 +0100

    init

---
+++ src/theme.tsx
@@ -0,0 +1,58 @@
+import { createTheme } from '@mui/material/styles'
+
+const customColors = {
+    dark: "#33475B",
+    white: "#F5F8FA",
+    light: "#F5F8FA",
+    grey: "#EAF0F6",
+    blue: "#239AB5",
+    orange: "#FF7A59"
+}
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: customColors.orange,
+        },
+        secondary: {
+            main: customColors.blue,
+        },
+        text: {
+            primary: customColors.dark,
+        },
+        info: {
+            main: customColors.grey,
+        },
+        background: {
+            default: customColors.white,
+            paper: customColors.light,
+        }
+    },
+    typography: {
+        fontFamily: 'Lexend Deca',
+        h1: {
+            fontSize: '24px',
+            lineHeight: '30px',
+            fontWeight: 700,
+            color: customColors.dark
+        },
+        h2: {
+            fontSize: '16px',
+            lineHeight: '24px',
+            fontWeight: 700,
+            color: customColors.dark
+        },
+        h3: {
+            fontSize: '14px',
+            lineHeight: '24px',
+            fontWeight: 400,
+            color: customColors.dark
+        },
+        body1: {
+            fontSize: '12px',
+            lineHeight: '24px',
+            fontWeight: 400,
+            color: customColors.dark
+        }
+    }
+})
